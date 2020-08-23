9a8e4f23f23e910cdeb55305e60883ebcefd09d4
digger888 <nulldivide@gmail.com>
2020-08-23 03:15:20 +0300

    verify

---
+++ verify/precache-manifest.0a7e7dc46cccf3c0314b4e8b3ed0f17b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03220a986282123610f707fcc7b9514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b781c6e1ff23ecf4442",
+    "url": "/static/css/main.c9cc19d6.chunk.css"
+  },
+  {
+    "revision": "693963fa5506a02805c1",
+    "url": "/static/js/2.40a54fd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.40a54fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b781c6e1ff23ecf4442",
+    "url": "/static/js/main.452f46d6.chunk.js"
+  },
+  {
+    "revision": "1d5f4af731d6e6ece5e0",
+    "url": "/static/js/runtime-main.b4d1c1e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
